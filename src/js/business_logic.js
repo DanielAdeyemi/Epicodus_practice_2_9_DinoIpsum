38d134f22c5d50f12e3771a2d6359867bbac37e4
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-09 14:39:26 -0800

    create BL, UI and HTML file which are work well
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>

--- src/js/business_logic.js
+++ src/js/business_logic.js
@@ -1,10 +1,13 @@
-// export default class Name {
-//   constructor(p1,p2) {
-//     this.p1 = p1;
-//     this.p2 = p2;
-//   }
-
-//   method() {
-//    //TODO
-//   }
-// }
+export default class Dino {
+  static getDino(par, words) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://dinoipsum.herokuapp.com/api/?format=json&words=${words}&paragraphs=${par}`;
+      request.onload = function() {
+        (this.status === 200) ? resolve(request.response): reject(request.response);
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '.././css/styles.css';
-// import Name from './business_logic.js';
+import Dino from './business_logic.js';
+
+$('.btn').click(function(event) {
+  event.preventDefault();
+  const par = parseInt($('#par').val());
+  const words = parseInt($('#words').val());
+  let promise = Dino.getDino(par, words);
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    $('#output').text(`${body}`);
+  }, function(error) {
+    $('#output').text(`There was an error processing your request: ${error}`);
+  });
+});
